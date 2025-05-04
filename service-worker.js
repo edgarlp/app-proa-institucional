04d5861e4c6c2c3afca1219826b51f5d2cd02707
edgarlp <133366543+edgarlp@users.noreply.github.com>
2025-05-04 14:25:45 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', e => {
+  console.log('Service Worker instalado');
+  e.waitUntil(self.skipWaiting());
+});
+self.addEventListener('activate', e => {
+  console.log('Service Worker activado');
+  return self.clients.claim();
+});
